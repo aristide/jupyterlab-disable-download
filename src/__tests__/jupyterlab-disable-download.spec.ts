d2de9c41afd5f25d84cbbd56ebfb55e8108fa4c3
unknown <aristide.mendoo@adncorp.com>
2023-12-18 18:07:58 +0000

    first commit

---
+++ src/__tests__/jupyterlab-disable-download.spec.ts
@@ -0,0 +1,9 @@
+/**
+ * Example of [Jest](https://jestjs.io/docs/getting-started) unit tests
+ */
+
+describe('jupyterlab-disable-download', () => {
+  it('should be tested', () => {
+    expect(1 + 1).toEqual(2);
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+/**
+ * Initialization data for the jupyterlab-disable-download extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'jupyterlab-disable-download:plugin',
+  autoStart: true,
+  activate: (app: JupyterFrontEnd) => {
+    console.log('JupyterLab extension jupyterlab-disable-download is activated!');
+  }
+};
+
+export default plugin;
