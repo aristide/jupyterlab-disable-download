635e0171cba2ad54705c15aa67662ccf3490c8cb
unknown <aristide.mendoo@adncorp.com>
2023-12-26 12:49:36 +0000

    disable download

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,18 @@ import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
+import { CommandRegistry } from '@lumino/commands';
 
+namespace CommandIDs {
+  export const copyToClipboard = 'notebook:copy-to-clipboard';
+  export const downloadFile = 'filebrowser:download';
+  export const downloadDoc = 'docmanager:download';
+  export const exportNotebook = 'notebook:export-to-format';
+}
+
+interface IMenuCommands {
+  _commands: { [index: string]: unknown };
+}
 /**
  * Initialization data for the jupyterlab-disable-download extension.
  */
@@ -10,9 +21,21 @@ const plugin: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab-disable-download:plugin',
   autoStart: true,
   activate: (app: JupyterFrontEnd) => {
-    console.log(
-      'JupyterLab extension jupyterlab-disable-download is activated!'
-    );
+    app.restored.then(() => {
+      const itemsToRemove = [
+        CommandIDs.copyToClipboard,
+        CommandIDs.downloadFile,
+        CommandIDs.downloadDoc,
+        CommandIDs.exportNotebook
+      ];
+      const commands = <IMenuCommands>(
+        (<CommandRegistry | IMenuCommands>app.commands)
+      );
+      app.commands;
+      itemsToRemove.forEach(item => {
+        delete commands._commands[item];
+      });
+    });
   }
 };
 
